120d963b783b5cedd1d51f41a5c444f0e3c04b1d
David Bilge <david.bilge@gmail.com>
2013-05-27 21:04:38 +0200

    Restored greek symbols μ and σ

--- src/jskills/numerics/GaussianDistribution.java
+++ src/jskills/numerics/GaussianDistribution.java
@@ -10,9 +10,9 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 import static jskills.numerics.MathUtils.square;
 import jskills.Rating;
+
 /**
- * Immutable representation of the Gaussian distribution of one variable. Not
- * normalized:
+ * Immutable representation of the Gaussian distribution of one variable. Not normalized:
  * 
  * <pre>
  *            1          -(x)^2 / (2)
@@ -23,112 +23,123 @@ import jskills.Rating;
  * Normalized:
  * 
  * <pre>
- *               1           -(x-\u03bc)^2 / (2*\u03c3^2)
+ *               1           -(x-μ)^2 / (2*σ^2)
  * P(x) = --------------- * e
- *        \u03c3 * sqrt(2*pi)
+ *        σ * sqrt(2*pi)
  * </pre>
  * 
  * @see http://mathworld.wolfram.com/NormalDistribution.html
  */
 
 public class GaussianDistribution {
-	/** 
-     * The Gaussian representation of a flat line. 
-     **/
+	/**
+	 * The Gaussian representation of a flat line.
+	 **/
 	public static final GaussianDistribution UNIFORM = fromPrecisionMean(0, 0);
-	/** The peak of the Gaussian, \u03bc **/
+	/** The peak of the Gaussian, μ **/
 	private final double mean;
-	/** The width of the Gaussian, \u03c3, where the height drops to max/e **/
+	/** The width of the Gaussian, σ, where the height drops to max/e **/
 	private final double standardDeviation;
-	/** The square of the standard deviation, \u03c3^2 **/
+	/** The square of the standard deviation, σ^2 **/
 	private final double variance;
 	// Precision and PrecisionMean are used because they make multiplying and
 	// dividing simpler (see the accompanying math paper for more details)
-	/** 1/\u03c3^2 **/
+	/** 1/σ^2 **/
 	private final double precision;
-	/** Precision times mean, \u03bc/\u03c3^2 **/
+	/** Precision times mean, μ/σ^2 **/
 	private final double precisionMean;
+
 	/**
-	 * The normalization constant multiplies the exponential and causes the
-	 * integral over (-Inf,Inf) to equal 1
+	 * The normalization constant multiplies the exponential and causes the integral over (-Inf,Inf) to equal 1
 	 * 
-	 * @return 1/sqrt(2*pi*\u03c3)
+	 * @return 1/sqrt(2*pi*σ)
 	 */
 	public double getNormalizationConstant() {
 		// Great derivation of this is at
 		// http://www.astro.psu.edu/~mce/A451_2/A451/downloads/notes0.pdf
 		return 1.0 / (sqrt(2 * PI) * standardDeviation);
 	}
+
 	/**
-     * Private constructor that sets everything at once.
-     * <p>
-     * Only allow other constructors to use this because if the public were to 
-     * mess up the relationship between the parameters, who knows what would 
-     * happen?
-     */
-	private GaussianDistribution(double mean, double standardDeviation, double variance, double precision, double precisionMean) {
-		
+	 * Private constructor that sets everything at once.
+	 * <p>
+	 * Only allow other constructors to use this because if the public were to mess up the relationship between the
+	 * parameters, who knows what would happen?
+	 */
+	private GaussianDistribution(double mean, double standardDeviation, double variance, double precision,
+			double precisionMean) {
+
 		this.mean = mean;
 		this.standardDeviation = standardDeviation;
 		this.variance = variance;
 		this.precision = precision;
 		this.precisionMean = precisionMean;
 	}
-	
+
 	public GaussianDistribution(double mean, double standardDeviation) {
-		this(mean, standardDeviation, square(standardDeviation), 1.0 / square(standardDeviation), mean / square(standardDeviation));
+		this(mean, standardDeviation, square(standardDeviation), 1.0 / square(standardDeviation), mean
+				/ square(standardDeviation));
 	}
-	
+
 	public GaussianDistribution(Rating rating) {
 		this(rating.getMean(), rating.getStandardDeviation());
 	}
-	
+
 	public GaussianDistribution(GaussianDistribution distribution) {
-		this(distribution.mean, distribution.standardDeviation, distribution.variance, distribution.precision, distribution.precisionMean);
+		this(distribution.mean, distribution.standardDeviation, distribution.variance, distribution.precision,
+				distribution.precisionMean);
 	}
-	
+
 	public static GaussianDistribution fromPrecisionMean(double precisionMean, double precision) {
-		return new GaussianDistribution(precisionMean / precision, sqrt(1.0 / precision), 1.0 / precision, precision, precisionMean);
+		return new GaussianDistribution(precisionMean / precision, sqrt(1.0 / precision), 1.0 / precision, precision,
+				precisionMean);
 	}
-	
+
 	public GaussianDistribution mult(GaussianDistribution other) {
 		return mult(this, other);
 	}
-	
+
 	public static GaussianDistribution mult(GaussianDistribution left, GaussianDistribution right) {
 		// Although we could use equations from
 		// http://www.tina-vision.net/tina-knoppix/tina-memo/2003-003.pdf
 		// for multiplication, the precision mean ones are easier to write :)
 		return fromPrecisionMean(left.precisionMean + right.precisionMean, left.precision + right.precision);
 	}
+
 	/** Computes the absolute difference between two Gaussians **/
 	public static double absoluteDifference(GaussianDistribution left, GaussianDistribution right) {
 		return max(abs(left.precisionMean - right.precisionMean), sqrt(abs(left.precision - right.precision)));
 	}
+
 	/** Computes the absolute difference between two Gaussians **/
 	public static double sub(GaussianDistribution left, GaussianDistribution right) {
 		return absoluteDifference(left, right);
 	}
-	
+
 	public static double logProductNormalization(GaussianDistribution left, GaussianDistribution right) {
-		if ((left.precision == 0) || (right.precision == 0)) return 0;
+		if ((left.precision == 0) || (right.precision == 0))
+			return 0;
 		double varianceSum = left.variance + right.variance;
 		double meanDifference = left.mean - right.mean;
 		double logSqrt2Pi = log(sqrt(2 * PI));
 		return -logSqrt2Pi - (log(varianceSum) / 2.0) - (square(meanDifference) / (2.0 * varianceSum));
 	}
-	
+
 	public static GaussianDistribution divide(GaussianDistribution numerator, GaussianDistribution denominator) {
-		return fromPrecisionMean(numerator.precisionMean - denominator.precisionMean, numerator.precision - denominator.precision);
+		return fromPrecisionMean(numerator.precisionMean - denominator.precisionMean, numerator.precision
+				- denominator.precision);
 	}
-	
+
 	public static double logRatioNormalization(GaussianDistribution numerator, GaussianDistribution denominator) {
-		if ((numerator.precision == 0) || (denominator.precision == 0)) return 0;
+		if ((numerator.precision == 0) || (denominator.precision == 0))
+			return 0;
 		double varianceDifference = denominator.variance - numerator.variance;
 		double meanDifference = numerator.mean - denominator.mean;
 		double logSqrt2Pi = log(sqrt(2 * PI));
-		return log(denominator.variance) + logSqrt2Pi - log(varianceDifference) / 2.0 + square(meanDifference) / (2 * varianceDifference);
+		return log(denominator.variance) + logSqrt2Pi - log(varianceDifference) / 2.0 + square(meanDifference)
+				/ (2 * varianceDifference);
 	}
+
 	/**
 	 * <pre>
 	 *               1          -(x)^2 / (2*stdDev^2)
@@ -136,15 +147,14 @@ public class GaussianDistribution {
 	 *           sqrt(2*pi)
 	 * </pre>
 	 * 
-	 * @param x
-	 *            the location to evaluate a normalized Gaussian at
-	 * @return the value at x of a normalized Gaussian centered at 0 of unit
-	 *         width.
+	 * @param x the location to evaluate a normalized Gaussian at
+	 * @return the value at x of a normalized Gaussian centered at 0 of unit width.
 	 * @see http://mathworld.wolfram.com/NormalDistribution.html
 	 */
 	public static double at(double x) {
 		return at(x, 0, 1);
 	}
+
 	/**
 	 * <pre>
 	 *               1          -(x)^2 / (2*stdDev^2)
@@ -152,10 +162,8 @@ public class GaussianDistribution {
 	 *           sqrt(2*pi)
 	 * </pre>
 	 * 
-	 * @param x
-	 *            the location to evaluate a normalized Gaussian at
-	 * @return the value at x of a normalized Gaussian centered at 0 of unit
-	 *         width.
+	 * @param x the location to evaluate a normalized Gaussian at
+	 * @return the value at x of a normalized Gaussian centered at 0 of unit width.
 	 * @see http://mathworld.wolfram.com/NormalDistribution.html
 	 */
 	public static double at(double x, double mean, double standardDeviation) {
@@ -164,23 +172,27 @@ public class GaussianDistribution {
 		double result = multiplier * expPart;
 		return result;
 	}
-	
+
 	public static double cumulativeTo(double x, double mean, double standardDeviation) {
 		double invsqrt2 = -0.7071067811865476;
 		double result = errorFunctionCumulativeTo(invsqrt2 * x);
 		return 0.5 * result;
 	}
-	
+
 	public static double cumulativeTo(double x) {
 		return cumulativeTo(x, 0, 1);
 	}
-	
+
 	private static double errorFunctionCumulativeTo(double x) {
-		// Derived from page 265 of Numerical Recipes 3rd Edition            
+		// Derived from page 265 of Numerical Recipes 3rd Edition
 		double z = abs(x);
 		double t = 2.0 / (2.0 + z);
 		double ty = 4 * t - 2;
-		double[] coefficients = {-1.3026537197817094, 0.6419697923564902, 0.019476473204185836, -0.00956151478680863, -9.46595344482036E-4, 3.66839497852761E-4, 4.2523324806907E-5, -2.0278578112534E-5, -1.624290004647E-6, 1.30365583558E-6, 1.5626441722E-8, -8.5238095915E-8, 6.529054439E-9, 5.059343495E-9, -9.91364156E-10, -2.27365122E-10, 9.6467911E-11, 2.394038E-12, -6.886027E-12, 8.94487E-13, 3.13092E-13, -1.12708E-13, 3.81E-16, 7.106E-15, -1.523E-15, -9.4E-17, 1.21E-16, -2.8E-17};
+		double[] coefficients = { -1.3026537197817094, 0.6419697923564902, 0.019476473204185836, -0.00956151478680863,
+				-9.46595344482036E-4, 3.66839497852761E-4, 4.2523324806907E-5, -2.0278578112534E-5, -1.624290004647E-6,
+				1.30365583558E-6, 1.5626441722E-8, -8.5238095915E-8, 6.529054439E-9, 5.059343495E-9, -9.91364156E-10,
+				-2.27365122E-10, 9.6467911E-11, 2.394038E-12, -6.886027E-12, 8.94487E-13, 3.13092E-13, -1.12708E-13,
+				3.81E-16, 7.106E-15, -1.523E-15, -9.4E-17, 1.21E-16, -2.8E-17 };
 		int ncof = coefficients.length;
 		double d = 0.0;
 		double dd = 0.0;
@@ -192,11 +204,13 @@ public class GaussianDistribution {
 		double ans = t * exp(-z * z + 0.5 * (coefficients[0] + ty * d) - dd);
 		return x >= 0.0 ? ans : (2.0 - ans);
 	}
-	
+
 	private static double InverseErrorFunctionCumulativeTo(double p) {
-		// From page 265 of numerical recipes                       
-		if (p >= 2.0) return -100;
-		if (p <= 0.0) return 100;
+		// From page 265 of numerical recipes
+		if (p >= 2.0)
+			return -100;
+		if (p <= 0.0)
+			return 100;
 		double pp = (p < 1.0) ? p : 2 - p;
 		double t = sqrt(-2 * log(pp / 2.0)); // Initial guess
 		double x = -0.70711 * ((2.30753 + t * 0.27061) / (1.0 + t * (0.99229 + t * 0.04481)) - t);
@@ -206,80 +220,88 @@ public class GaussianDistribution {
 		}
 		return p < 1.0 ? x : -x;
 	}
-	
+
 	public static double inverseCumulativeTo(double x, double mean, double standardDeviation) {
 		// From numerical recipes, page 320
 		return mean - sqrt(2) * standardDeviation * InverseErrorFunctionCumulativeTo(2 * x);
 	}
-	
+
 	public static double inverseCumulativeTo(double x) {
 		return inverseCumulativeTo(x, 0, 1);
 	}
-	
+
 	@Override
 	public String toString() {
 		// Debug help
-		return String.format("Mean(\u03bc)=%f, Std-Dev(\u03c3)=%f", mean, standardDeviation);
+		return String.format("Mean(μ)=%f, Std-Dev(σ)=%f", mean, standardDeviation);
 	}
-	
+
 	@java.lang.Override
 	@java.lang.SuppressWarnings("all")
 	public boolean equals(final java.lang.Object o) {
-		if (o == this) return true;
-		if (!(o instanceof GaussianDistribution)) return false;
-		final GaussianDistribution other = (GaussianDistribution)o;
-		if (!other.canEqual((java.lang.Object)this)) return false;
-		if (java.lang.Double.compare(this.getMean(), other.getMean()) != 0) return false;
-		if (java.lang.Double.compare(this.getStandardDeviation(), other.getStandardDeviation()) != 0) return false;
-		if (java.lang.Double.compare(this.getVariance(), other.getVariance()) != 0) return false;
-		if (java.lang.Double.compare(this.getPrecision(), other.getPrecision()) != 0) return false;
-		if (java.lang.Double.compare(this.getPrecisionMean(), other.getPrecisionMean()) != 0) return false;
+		if (o == this)
+			return true;
+		if (!(o instanceof GaussianDistribution))
+			return false;
+		final GaussianDistribution other = (GaussianDistribution) o;
+		if (!other.canEqual((java.lang.Object) this))
+			return false;
+		if (java.lang.Double.compare(this.getMean(), other.getMean()) != 0)
+			return false;
+		if (java.lang.Double.compare(this.getStandardDeviation(), other.getStandardDeviation()) != 0)
+			return false;
+		if (java.lang.Double.compare(this.getVariance(), other.getVariance()) != 0)
+			return false;
+		if (java.lang.Double.compare(this.getPrecision(), other.getPrecision()) != 0)
+			return false;
+		if (java.lang.Double.compare(this.getPrecisionMean(), other.getPrecisionMean()) != 0)
+			return false;
 		return true;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public boolean canEqual(final java.lang.Object other) {
 		return other instanceof GaussianDistribution;
 	}
-	
+
 	@java.lang.Override
 	@java.lang.SuppressWarnings("all")
 	public int hashCode() {
 		final int PRIME = 31;
 		int result = 1;
 		final long $mean = java.lang.Double.doubleToLongBits(this.getMean());
-		result = result * PRIME + (int)($mean >>> 32 ^ $mean);
+		result = result * PRIME + (int) ($mean >>> 32 ^ $mean);
 		final long $standardDeviation = java.lang.Double.doubleToLongBits(this.getStandardDeviation());
-		result = result * PRIME + (int)($standardDeviation >>> 32 ^ $standardDeviation);
+		result = result * PRIME + (int) ($standardDeviation >>> 32 ^ $standardDeviation);
 		final long $variance = java.lang.Double.doubleToLongBits(this.getVariance());
-		result = result * PRIME + (int)($variance >>> 32 ^ $variance);
+		result = result * PRIME + (int) ($variance >>> 32 ^ $variance);
 		final long $precision = java.lang.Double.doubleToLongBits(this.getPrecision());
-		result = result * PRIME + (int)($precision >>> 32 ^ $precision);
+		result = result * PRIME + (int) ($precision >>> 32 ^ $precision);
 		final long $precisionMean = java.lang.Double.doubleToLongBits(this.getPrecisionMean());
-		result = result * PRIME + (int)($precisionMean >>> 32 ^ $precisionMean);
+		result = result * PRIME + (int) ($precisionMean >>> 32 ^ $precisionMean);
 		return result;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public double getMean() {
 		return this.mean;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public double getStandardDeviation() {
 		return this.standardDeviation;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public double getVariance() {
 		return this.variance;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public double getPrecision() {
 		return this.precision;
 	}
-	
+
 	@java.lang.SuppressWarnings("all")
 	public double getPrecisionMean() {
 		return this.precisionMean;
